3e7f09b6cf476f0098da5c57b199a4910f24e5ad
Erik Nuber <erik.nuber@yahoo.com>
2020-10-24 10:42:42 -0700

    updated app from class to functional component and changed to hooks

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 import history from '../history';
 import Geocode from 'react-geocode';
@@ -9,104 +9,97 @@ import Header from './Header';
 import ZipcodeInput from './Zipcode_Input';
 import Keys from '../config.json';
 
-class App extends React.Component {
-
-    state = {
-        currentWeather: {},
-        dayClickedWeather: {},
-        lat: null,
-        lng: null,
-        city: null,
-        state: null,
-        zip: null,
-        error: false
-    };
+const App = () => {
+    const [currentWeather, setCurrentWeather] = useState({});
+    const [dayClickedWeather, setdDayClickedWeather] = useState({});
+    const [lat, setLat] = useState(null);
+    const [lng, setLng] = useState(null);
+    const [city, setCity] = useState(null);
+    const [state, setStateOfUSA] = useState(null);
+    const [zip, setZip] = useState(null);
+    const [error, setError] = useState(false);
 
     //uses a call to google api to get lat and long from zipcode. Need these to properly make the call to weather api
-    getLocation = async zipcode => {
-        if (!this.state.error) {
+    const getLocation = async zipcode => {
+        if (!error) {
             Geocode.setApiKey(Keys.keys[0].google);
             await Geocode.fromAddress(zipcode).then(
                 response => {
                     const {lat, lng} = response.results[0].geometry.location;
                     const city = response.results[0].address_components[1].long_name;
                     const state = response.results[0].address_components[3].short_name;
-                    this.setState({
-                        lat: lat,
-                        lng: lng,
-                        city: city,
-                        state: state,
-                        zip: zipcode,
-                        error: false
-                    });
+                    setLat(lat);
+                    setLng(lng);
+                    setCity(city);
+                    setStateOfUSA(state);
+                    setZip(zipcode);
+                    setError(false);
                 },
                 error => {
-                    this.setState({error: true});
+                    setError(true);
                 }
             )
         }
     };
 
     //this happens after the getLocation finishes making it's call so we can use the lat and lng to get weather data
-    getWeather = async () => {
-        if (!this.state.error) {
+    const getWeather = async () => {
+        if (!error) {
             const response = await openweatherOnecall.get('', {
                 params: {
-                    lat: this.state.lat,
-                    lon: this.state.lng,
+                    lat: lat,
+                    lon: lng,
                     exclude: "minutely",
                     units: "imperial",
                     appid: Keys.keys[0].openweather
                 }
             });
-            this.setState({currentWeather: response.data});
+            setCurrentWeather(response.data);
         }
     };
 
-    onSearchSubmit = async (zipcode, error) => {
+    const onSearchSubmit = async (zipcode, error) => {
         history.push('/');
-        await this.setState({error: error});
-        await this.getLocation(zipcode);
-        await this.getWeather(zipcode);
+        await setError(error);
+        await getLocation(zipcode);
+        await getWeather(zipcode);
     };
 
-    onDayClick = (dayClicked, day) => {
-        this.setState({dayClickedWeather: dayClicked});
+    const onDayClick = (dayClicked, day) => {
+        setdDayClickedWeather(dayClicked);
         history.push(`/${day}`);
     };
 
-    render() {
-        return (
-            <div className="ui container" style={{marginTop: '10px'}}>
-                <Router history={history}>
-                    <Header/>
-                    <ZipcodeInput onSubmit={this.onSearchSubmit}/>
-                    <Switch>
-                        <Route path="/" exact render={() =>
-                            <ShowWeather
-                                allWeather={this.state.currentWeather}
-                                city={this.state.city}
-                                state={this.state.state}
-                                zip={this.state.zip}
-                                error={this.state.error}
-                                clickedADay={this.onDayClick}
-                            />}
-                        />
-                        <Route path="/:day" render={() =>
-                            <ShowDay
-                                dayWeather={this.state.dayClickedWeather}
-                                city={this.state.city}
-                                state={this.state.state}
-                                zip={this.state.zip}
-                                error={this.state.error}
-                                clickedADay={this.onDayClick}
-                            />}
-                        />
-                    </Switch>
-                </Router>
-            </div>
-        );
-    }
-}
+    return (
+        <div className="ui container" style={{marginTop: '10px'}}>
+            <Router history={history}>
+                <Header/>
+                <ZipcodeInput onSubmit={onSearchSubmit}/>
+                <Switch>
+                    <Route path="/" exact render={() =>
+                        <ShowWeather
+                            allWeather={currentWeather}
+                            city={city}
+                            state={state}
+                            zip={zip}
+                            error={error}
+                            clickedADay={onDayClick}
+                        />}
+                    />
+                    <Route path="/:day" render={() =>
+                        <ShowDay
+                            dayWeather={dayClickedWeather}
+                            city={city}
+                            state={state}
+                            zip={zip}
+                            error={error}
+                            clickedADay={onDayClick}
+                        />}
+                    />
+                </Switch>
+            </Router>
+        </div>
+    );
+};
 
 export default App;
